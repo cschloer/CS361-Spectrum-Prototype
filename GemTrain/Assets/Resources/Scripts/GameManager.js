6034ee87bd953e2de7040339ca089dbaf2a0fc5a
cschloer <conrad.schloer@gmail.com>
2014-10-02 22:23:54 -0400

    Adding Gem Train files

---
+++ GemTrain/Assets/Resources/Scripts/GameManager.js
@@ -0,0 +1,51 @@
+﻿// Tom Wexler
+// Example program to help you get started with your project.
+
+
+var gemFolder : GameObject;	// This will be an empty game object used for organizing objects in the hierarchy pane.
+var gems : Array;			// This array will hold the gem objects that are created.
+var gemType : int; 		// The next gem type to be created.
+
+
+
+// Called once when the script is created.
+function Start () {
+	gemFolder = new GameObject();  
+	gemFolder.name = "Gems";
+	gems = new Array();
+	gemType = 1;
+}
+
+// Called every frame.
+function Update () {
+	if (Input.GetMouseButtonUp(0)) { // If the user releases the mouse button, figure out where the mouse is and spawn a gem.
+		var worldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		var mouseX = worldPos.x;
+		var mouseY = worldPos.y;
+		print("you just clicked at "+mouseX+" "+mouseY);
+		addGem(mouseX, mouseY);
+	}
+}
+
+function addGem(x : float , y : float) {
+	var gemObject = new GameObject();					// Create a new empty game object that will hold a gem.
+	var gemScript = gemObject.AddComponent("gem");		// Add the gem.js script to the object.
+														// We can now refer to the object via this script.
+	gemScript.transform.parent = gemFolder.transform;	// Set the gem's parent object to be the gem folder.
+	gemScript.transform.position = Vector3(x,y,0);		// Position the gem at x,y.								
+	
+	gemScript.init(gemType);							// Initialize the gem script.
+	
+	gems.Add(gemScript);								// Add the gem to the gems array for future access.
+	gemScript.name = "Gem "+gems.length;				// Give the gem object a name in the Hierarchy pane.
+	
+	gemType = (gemType%4) + 1;							
+}
+
+function OnGUI () {
+	if (GUI.Button (Rect (10,400,100,30), "Self Destruct")) 
+		print("omg you pressed a button"); 
+		// Printing goes to the Console pane.  
+		// If an object doesn't extend monobehavior (it will by default, but not if you define your own class), calling print won't do anything.  
+		// Make sure "Collapse" isn't selected in the Console pane if you want to see duplicate messages.
+}
---
+++ GemTrain/Assets/Resources/Scripts/gem.js
@@ -0,0 +1,16 @@
+﻿var model : gemModel;
+
+function init(gemType : int) {
+	var modelObject = GameObject.CreatePrimitive(PrimitiveType.Quad);	// Create a quad object for holding the gem texture.
+	model = modelObject.AddComponent("gemModel");						// Add a gemModel script to control visuals of the gem.
+	model.gemType = gemType;
+	
+	model.transform.parent = transform;									// Set the model's parent to the gem (this object).
+	model.transform.localPosition = Vector3(0,0,0);						// Center the model on the parent.
+	model.name = "Gem Model";											// Name the object.
+	
+	model.renderer.material.mainTexture = Resources.Load("Textures/gem"+gemType, Texture2D);	// Set the texture.  Must be in Resources folder.
+	model.renderer.material.color = Color(1,1,1);												// Set the color (easy way to tint things).
+	model.renderer.material.shader = Shader.Find ("Transparent/Diffuse");						// Tell the renderer that our textures have transparency. 
+}
+
