b22fb7015f08713bf258a85f131637d7f7d33a96
gfishel <gabriel@ip-91-205.wireless.oberlin.edu>
2014-10-05 15:02:11 -0400

    Level Design Retry
    See previous commit for information about new elements.

--- Spectrum/Assets/Resources/Scripts/GameManager.js
+++ Spectrum/Assets/Resources/Scripts/GameManager.js
@@ -1,16 +1,25 @@
-﻿var characterFolder : GameObject;	// This will be an empty game object used for organizing objects in the hierarchy pane.
-var character : Character;			// This array will hold the character objects that are created.
-var monsterFolder : Array;
+﻿var characterFolder : GameObject;	// This will be an empty game object used for organizing heroes in the hierarchy pane.
+var monsterFolder : GameObject;		// This will be an empty game object used for organizing monsters in the hierarchy pane.
+var tileFolder : GameObject;		// This will be an empty game object used for organizing tiles in the hierarchy pane.
+var character : Character;			// This is the hero character.
+var monsters : Array;				// This array holds monsters.
+var tiles : Array;					// This array holds tiles.
 
 
 // Called once when the script is created.
 function Start () {
 	characterFolder = new GameObject();  
 	characterFolder.name = "Character";
-	monsterFolder = new Array();
-//	monsterFolder.name = "Monsters";
+	monsterFolder = new GameObject();
+	monsterFolder.name = "Monsters";
+	monsters = new Array();
+	tiles = new Array();
+
 	addCharacter(0,0);
 	addMonster(4, 3, character);
+	
+	//protolevelInit();
+	
 }
 
 // Called every frame.
@@ -18,6 +27,8 @@ function Update () {
 
 }
 
+
+// Adds: These functions add certain elements.
 function addCharacter(x : float , y : float) {
 	var characterObject = new GameObject();					// Create a new empty game object that will hold a character.
 	var characterScript = characterObject.AddComponent("Character");		// Add the character.js script to the object.
@@ -32,12 +43,33 @@ function addCharacter(x : float , y : float) {
 
 	
 }
+
 function addMonster(x : float, y :float, c : Character){
 	var monsterObject = new GameObject();					// Create a new empty game object that will hold a character.
 	var monsterScript = monsterObject.AddComponent("Monster");		// Add the character.js script to the object.
+	
+	monsterScript.transform.parent = monsterFolder.transform;
 	monsterScript.transform.position = Vector3(x,y,0);		// Position the character at x,y.								
 	
 	monsterScript.init(c);
-	monsterFolder.Add(monsterScript);
-	monsterScript.name = "Monster";
+	monsters.Add(monsterScript);
+	monsterScript.name = "Monster"+ monsters.length;
+}
+
+function addTile(x : float, y :float, t : String){
+	var tileObject = new GameObject();						// Create a new empty game object that will hold a character.
+	var tileScript = tileObject.AddComponent("Tile");		// Add the character.js script to the object.
+	tileScript.transform.position = Vector3(x,y,0);		// Position the character at x,y.								
+	
+	tileScript.init(t);
+	tileFolder.Add(tileScript);
+	tileScript.name = "Tile" + tiles.length;
+}
+
+// ProtolevelInit
+// Initiates the prototype level.
+function protolevelInit(){
+  
+  
+  
 }
