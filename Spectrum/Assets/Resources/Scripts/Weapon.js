32f00c51b3e8ccc5fcd1ea2c64bcc72fa131d920
Hertafeld <ehertafe@oberlin.edu>
2014-10-05 16:47:58 -0400

    Added Weapon sprite

--- Spectrum/Assets/Resources/Scripts/Weapon.js
+++ Spectrum/Assets/Resources/Scripts/Weapon.js
@@ -8,9 +8,9 @@ public class Weapon extends MonoBehaviour{
 		model = modelObject.AddComponent("WeaponModel");						// Add a gemModel script to control visuals of the gem.
 			
 		model.transform.parent = owner.model.transform;									// Set the model's parent to the gem (this object).
-		model.transform.localPosition = Vector3(0,0,0);						// Center the model on the parent.
+		model.transform.localPosition = Vector3(0,1,0);						// Center the model on the parent.
 		model.name = "Weapon Model";											// Name the object.
-		model.renderer.material.mainTexture = Resources.Load("Textures/stick", Texture2D);	// Set the texture.  Must be in Resources folder.
+		model.renderer.material.mainTexture = Resources.Load("Textures/stick2", Texture2D);	// Set the texture.  Must be in Resources folder.
 		model.renderer.material.color = Color(1,1,1);												// Set the color (easy way to tint things).
 		model.renderer.material.shader = Shader.Find ("Transparent/Diffuse");						// Tell the renderer that our textures have transparency. 
 	
